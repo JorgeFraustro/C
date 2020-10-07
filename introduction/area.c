09d05c7cacc236e2450e3944c0d9ed4921844a83
JorgeFraustro <jorgefraustro9.52@gmail.com>
2020-10-07 13:34:57 -0500

    Archivos nuevos
    Agregué archivos nuevos al repositorio.

--- introduction/area.c
+++ introduction/area.c
@@ -3,12 +3,12 @@
 
 #define PI 3.1416 //Definiendo una constante
 
-int main(){
+void main(){
 	float area, radio; //Declarando variables de tipo flotante
 	radio = 5; //Asignando valor a variable-radio
 	area = PI * pow(5,2); //Asignando el resultado de la operación a variable area
 	printf("Area\n"); //Imprimieno titulo
 	//Imprimiendo resultado
-	printf("%s%f\n\n", "Area de Circulo con radio 5: %f", area);
-	return 0;
+	printf("Area de Circulo con radio 5: %f", area);
+	return 0; 
 }
